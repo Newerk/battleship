e35134732b3d04c98e4a369a5285a273d157d54e
newerk <juniusowens@yahoo.com>
2023-04-10 13:01:23 -0400

    Add Webpack, Babel & Jest to Project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    
+    optimization: {
+        runtimeChunk: 'single',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battleship',
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+
+        ],
+    },
+
+};
