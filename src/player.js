8d95de80ede6748be87f4569902bca8d8a79547b
newerk <juniusowens@yahoo.com>
2023-04-17 17:14:19 -0400

    Add Boilerplate Code for Player Function

--- src/player.js
+++ src/player.js
@@ -1,9 +1,28 @@
-// import { Gameboard } from "./gameboard"
+import { Gameboard } from "./gameboard"
 
-export const Player = () => {
+export const Player = (enemyBoard) => {
+    let personalBoard = Gameboard();
+
+    function sumOfSpotsTaken() {
+        return Object.values(personalBoard.ships).reduce((prev, current) => prev.length + current.length, 0)
+    }
+
+
     return {
-        attack(coord, enemyBoard) {
-            enemyBoard.recieveAttack(coord)
-        }
+        attack(coord) {
+            enemyBoard.receiveAttack(coord);
+        },
+        randomlyPlaceShips() {//for the CPU to place ships in valid spots when called. select random coords(in a limited alphabetical and numerical range A-J,1-10) and X/Y axis
+            while (enemyBoard.shipOccupiedCoords.length < sumOfSpotsTaken()) {
+                for (let i = 0; i < Object.keys(enemyBoard.ships).length; i++) {
+                    enemyBoard.placeShip(enemyBoard.ships[Object.keys(enemyBoard.ships)[i]], ''/*random coord*/, ''/*random axis*/)
+                }
+
+            }
+        },
+        board: personalBoard.board,
+        sumOfSpotsTaken
+
     }
 }
+
