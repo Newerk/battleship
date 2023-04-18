25e90bc685338f4e78eaadafdd04b1f8ec8f950f
newerk <juniusowens@yahoo.com>
2023-04-18 11:42:09 -0400

    Add Functions to Create Rand Coord & Axis

--- src/player.js
+++ src/player.js
@@ -7,6 +7,14 @@ export const Player = (enemyBoard) => {
         return Object.values(personalBoard.ships).reduce((prev, { length }) => prev + length, 0)
     }
 
+    function randomCoordinate() {
+        return "ABCDEFGHIJ"[Math.floor(10 * Math.random())] + Math.floor(Math.random() * (11 - 1) + 1);
+    }
+
+    function randomAxis() { //randomly return X or Y
+        return "XY"[Math.floor(2 * Math.random())];
+    }
+
 
     return {
         attack(coord) {
@@ -15,14 +23,13 @@ export const Player = (enemyBoard) => {
         randomlyPlaceShips() {//for the CPU to place ships in valid spots when called. select random coords(in a limited alphabetical and numerical range A-J,1-10) and X/Y axis
             while (enemyBoard.shipOccupiedCoords.length < sumOfSpotsTaken()) {
                 for (let i = 0; i < Object.keys(enemyBoard.ships).length; i++) {
-                    enemyBoard.placeShip(enemyBoard.ships[Object.keys(enemyBoard.ships)[i]], ''/*random coord*/, ''/*random axis*/)
+                    enemyBoard.placeShip(enemyBoard.ships[Object.keys(enemyBoard.ships)[i]], randomCoordinate(), randomAxis())
                 }
 
             }
         },
         board: personalBoard.board,
-        sumOfSpotsTaken
-
+        sumOfSpotsTaken,
     }
 }
 
