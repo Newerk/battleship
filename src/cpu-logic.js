ebf47151f02958a08f5c86fa9bbaa8800b1b874d
newerk <juniusowens@yahoo.com>
2023-04-20 16:27:08 -0400

    Add Potential Structure 4 cpuAttack()

--- src/cpu-logic.js
+++ src/cpu-logic.js
@@ -1,3 +1,6 @@
+import { Player } from "./player"
+
+
 /* 
 ---DETERMINES HOW THE CPU WILL DECIDE TO PLAY A TURN---
 
@@ -9,7 +12,7 @@ space away from the hit location, exmaple:
 cpu attacks B3, it was a hit
 next turn the CPU can either attack B2,B4,A3,C3
 this is where keeping track of hit locations become important
-depending on which spot was hit on the next turn after B3 will determine if it keep trying vertical postions or horizontal positions
+depending on which spot was hit on the next turn after B3, will determine if it keeps trying vertical postions or horizontal positions
 
 cpu does B3->B4. what will be the computer's logic in this situation? since the cpu traveled down vertically from B3 to B4, it will attempt B5 next IF B5 isnt already on the hit or miss
 list. 
@@ -18,11 +21,18 @@ Next turn:
 cpu tries B5, turns out it was a miss.
 
 Next turn:
-The cpu will now attempt B2 next. The cpu will continue along a single axis until a ship is sunk. in this case, after the cpu attacks B2, 
+The cpu will now attempt B2. The cpu will continue along a single axis until a ship is sunk. in this case, after the cpu attacks B2, 
 it will have sunk a ship (submarine w/ a length of 3)
 The submarine occupied coordinates B2->B3->B4
 
 the cpu will check after each hit (not miss), if a ship isSunk(), 
 
 after a ship was sunk, the cpu will now go back to attacking random locations until another ship is hit, and go through this entire process again
-*/
+*/ 
+
+export function cpuAttack(enemyBoard) {
+
+    if (/*enemyBoard.recieveAttack() === false, recusrsivly run the cpuAttack() again */"") {
+        return cpuAttack(enemyBoard)
+    }
+}
