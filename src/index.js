affb0ff47b1861027931390fc0dbcc31cb963aec
newerk <juniusowens@yahoo.com>
2023-05-01 13:03:36 -0400

    Add Notes for Character Selection Screen

--- src/index.js
+++ src/index.js
@@ -8,3 +8,22 @@ document.body.appendChild(crt);
 
 // DOM().loadTitleScreen();
 DOM().loadInGameScreen();
+
+
+/*use localStorage to keep track of the characters used in the game.
+if there are not characters selected, load a character selection page (after the user presses enter on title screen).
+also allow the user the randomly choose characters. Has to make sure that the player and 
+cpu dont use the same characters(including their variants)
+
+     PLAYER               COMPUTER
+  [random btn]          [random btn]  
+_________________     _________________   
+| | | | | | | | |     | | | | | | | | |    
+-----------------     -----------------    
+| | | | | | | | |     | | | | | | | | |    
+-----------------     -----------------    
+
+                            [continue]
+
+my planned layout for the charcter selection screen
+*/
