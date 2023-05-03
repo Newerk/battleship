56607e3dcb765adf6c151ff0aa35812e056b8826
newerk <juniusowens@yahoo.com>
2023-05-03 15:57:16 -0400

    Add Hover Effect to Char Sprites

---
+++ src/select-random-character.js
@@ -0,0 +1,5 @@
+/*updates the large picture of the character selected. This
+will also show the Name and short description of the character
+below the large picture. The selected character on the grid 
+will also have its picture brightened().
+*/
