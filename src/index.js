01d38d47c45ccf8e33e87e9710b7c8d1f77a46a7
newerk <juniusowens@yahoo.com>
2023-07-12 12:43:37 -0400

    Code Clean-up: Remove Comments Used as Notes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import { DOM } from './DOM';
 import './local-storage'
 import { audioController } from './audio-control';
 
-//crt effect
 const crt = document.createElement('div');
 crt.id = 'crt';
 document.body.appendChild(crt);
 
 audioController();
 
-//Make a fade in and out to black when changing screens. Perhaps this can be done w/ pure CSS
 DOM().loadTitleScreen();
-// DOM().loadCharacterSelectScreen();
-// DOM().loadInGameScreen();
-// DOM().loadGameOverScreen();
 
